1b4b1afd2c6face7b497350023a3d87b57ad7368
dengjinlang <dengjinlang@aiiiin.com>
2026-03-27 10:26:42 +0800

    Initial Commit

---
+++ EchoPulse/EchoPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoPulseApp.swift
+//  EchoPulse
+//
+//  Created by dj on 2026/3/27.
+//
+
+import SwiftUI
+
+@main
+struct EchoPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
